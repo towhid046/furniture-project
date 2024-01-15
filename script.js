739821a3f785bea4b518909a4e9439df17f36c49
towhid046 <towhidmorol46@gmail.com>
2024-01-15 14:46:21 +0600

    old project commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carouselContainer').slick({ 
+        autoplay: false,
+        autoplaySpeed: 1000,
+        dots: true,
+        prevArrow: `<button type="button" class="slick-prev"><</button>`,
+        nextArrow: `<button type="button" class="slick-next">></button>`,
+
+    });
+
+});
